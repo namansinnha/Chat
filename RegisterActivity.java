e52e3611be96072ad915e5b353ea36d11604d7aa
namansinnha <42332974+namansinnha@users.noreply.github.com>
2019-11-28 15:37:52 +0530

    Update RegisterActivity.java

--- RegisterActivity.java
+++ RegisterActivity.java
@@ -1,30 +1,3 @@
-package com.developer.gkweb.knowlocation.Activities;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatTextView;
-import androidx.appcompat.widget.Toolbar;
-import android.app.ProgressDialog;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
-import com.developer.gkweb.knowlocation.R;
-import com.developer.gkweb.knowlocation.Util.AsteriskPasswordTransformationMethod;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.textfield.TextInputEditText;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.HashMap;
-
-import es.dmoral.toasty.Toasty;
-
 public class RegisterActivity extends AppCompatActivity {
 
     //vars

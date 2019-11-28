53b92f3c895c4ff860504a4740d2abe92702fb65
namansinnha <42332974+namansinnha@users.noreply.github.com>
2019-11-28 15:39:15 +0530

    Update UserAdapter.java

--- UserAdapter.java
+++ UserAdapter.java
@@ -1,31 +1,3 @@
-package com.developer.gkweb.knowlocation.Adapter;
-
-import android.content.Context;
-import android.content.Intent;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.bumptech.glide.Glide;
-import com.developer.gkweb.knowlocation.Activities.MessageActivity;
-import com.developer.gkweb.knowlocation.Model.Chat;
-import com.developer.gkweb.knowlocation.Model.User;
-import com.developer.gkweb.knowlocation.R;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.List;
-
 public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
 
     //vars:

2c313115706f3e1853776ee2ac4efe2ef194392f
namansinnha <42332974+namansinnha@users.noreply.github.com>
2019-11-28 15:30:01 +0530

    Update MyFirebaseMessaging.java

--- MyFirebaseMessaging.java
+++ MyFirebaseMessaging.java
@@ -1,25 +1,3 @@
-package com.developer.gkweb.knowlocation.Notification;
-
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import android.os.Build;
-import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.core.app.NotificationCompat;
-
-import com.developer.gkweb.knowlocation.Activities.MessageActivity;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.messaging.FirebaseMessagingService;
-import com.google.firebase.messaging.RemoteMessage;
-
 public class MyFirebaseMessaging extends FirebaseMessagingService {
 
     @Override

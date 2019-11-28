7b6b21fc427a40b7169c57e2fdde16c3a177cd9f
namansinnha <42332974+namansinnha@users.noreply.github.com>
2019-11-28 15:13:02 +0530

    Interface

---
+++ APIService.java
@@ -0,0 +1,25 @@
+package com.developer.gkweb.knowlocation.Interface;
+
+import com.developer.gkweb.knowlocation.Notification.MyResponse;
+import com.developer.gkweb.knowlocation.Notification.Sender;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
+public interface APIService {
+
+    @Headers(
+            {
+
+                    "Content-Type:application/json",
+                    "Authorization:key=AAAArJBqqXI:APA91bGpNEhcl-jjVx1Y0gBTTD8XPpuo3FIQ9VWVu3ImXxZysqRrhFl0EzVFjFDq1dEDZgbSPH-6khWF0GjqC1PuzeIhbFd0tufmDXOG2Mi1KjjT_tbMi401cgNeComdgtlwAp5mZP0Q"
+
+            }
+    )
+
+    @POST ("fcm/send")
+    Call<MyResponse> sendNotification(@Body Sender body);
+
+}

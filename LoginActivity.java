fa089610f2a255a86a6f108154eeb7c684406b79
namansinnha <42332974+namansinnha@users.noreply.github.com>
2019-11-28 15:28:02 +0530

    Update LoginActivity.java

--- LoginActivity.java
+++ LoginActivity.java
@@ -1,29 +1,3 @@
-package com.developer.gkweb.knowlocation.Activities;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.app.ProgressDialog;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
-import com.developer.gkweb.knowlocation.Util.AsteriskPasswordTransformationMethod;
-import com.developer.gkweb.knowlocation.R;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.textfield.TextInputEditText;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import es.dmoral.toasty.Toasty;
-
 public class LoginActivity extends AppCompatActivity {
 
     //vars

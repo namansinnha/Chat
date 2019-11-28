d19686cedf7ea1dc7b672fe891541c8711c063da
namansinnha <42332974+namansinnha@users.noreply.github.com>
2019-11-28 15:39:39 +0530

    Update UsersFragment.java

--- UsersFragment.java
+++ UsersFragment.java
@@ -1,35 +1,3 @@
-package com.developer.gkweb.knowlocation.Fragment;
-
-
-import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.EditText;
-
-import com.developer.gkweb.knowlocation.Adapter.UserAdapter;
-import com.developer.gkweb.knowlocation.Model.User;
-import com.developer.gkweb.knowlocation.R;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class UsersFragment extends Fragment {
 
     //vars:

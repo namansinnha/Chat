1f0f89ff02ac4d0206206eeee708faa3e80df23e
namansinnha <42332974+namansinnha@users.noreply.github.com>
2019-11-28 15:25:19 +0530

    Update ChatsFragment.java

--- ChatsFragment.java
+++ ChatsFragment.java
@@ -1,34 +1,3 @@
-package com.developer.gkweb.knowlocation.Fragment;
-
-import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import com.developer.gkweb.knowlocation.Adapter.UserAdapter;
-import com.developer.gkweb.knowlocation.Model.Chat;
-import com.developer.gkweb.knowlocation.Model.ChatList;
-import com.developer.gkweb.knowlocation.Model.User;
-import com.developer.gkweb.knowlocation.Notification.Token;
-import com.developer.gkweb.knowlocation.R;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.iid.FirebaseInstanceId;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class ChatsFragment extends Fragment {
 
     //vars:
